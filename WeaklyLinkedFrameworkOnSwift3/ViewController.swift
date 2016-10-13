8f323458b24ddeeda64a3a8b16ea622628d8b858
Víctor Pimentel <vpimentel@tuenti.com>
2016-10-13 16:28:25 +0200

    Add minimum code to cause crash.

--- WeaklyLinkedFrameworkOnSwift3/ViewController.swift
+++ WeaklyLinkedFrameworkOnSwift3/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import CallKit
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+
+		print("Crash here :(")
 	}
 
 	override func didReceiveMemoryWarning() {
